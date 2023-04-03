9e2a88fcee182e0349e90b0fe66d4e1e08d802d5
tisweekendboot <tisweekendboot@gmail.com>
2023-04-03 11:32:38 +0900

    file upload

--- gallery/src/main/java/com/jjang051/gallery/dto/GalleryDto.java
+++ gallery/src/main/java/com/jjang051/gallery/dto/GalleryDto.java
@@ -1,6 +1,7 @@
 package com.jjang051.gallery.dto;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class GalleryDto {
@@ -8,6 +9,7 @@ public class GalleryDto {
   int no;
   private String title;
   private String contents;
+  private MultipartFile file; // insert.html에 file의 name
   private String original;
   private String renamed;
 }
