bbab7695f5c4e42836d9b92f72c7b8453a25c063
tisweekendboot <tisweekendboot@gmail.com>
2023-04-04 11:37:02 +0900

    json

--- todo/src/main/java/com/jjang051/todo/controller/TodoController.java
+++ todo/src/main/java/com/jjang051/todo/controller/TodoController.java
@@ -1,13 +1,31 @@
 package com.jjang051.todo.controller;
 
+import com.jjang051.todo.dto.TodoDto;
+import com.jjang051.todo.service.TodoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TodoController {
 
+  @Autowired
+  TodoService todoService;
+
   @GetMapping("/index")
   public String home() {
     return "/todo/index";
+  }
+
+  @PostMapping("/insert")
+  @ResponseBody
+  public List<TodoDto> insertTodo(TodoDto todoDto) {
+    int result = todoService.insertTodo(todoDto);
+    List<TodoDto> todoList = todoService.getAllTodo(todoDto.getPickedDate());
+    return todoList;
   }
 }
---
+++ todo/src/main/java/com/jjang051/todo/service/TodoService.java
@@ -0,0 +1,10 @@
+package com.jjang051.todo.service;
+
+import com.jjang051.todo.dto.TodoDto;
+import java.util.List;
+
+public interface TodoService {
+  public int insertTodo(TodoDto todoDto);
+
+  public List<TodoDto> getAllTodo(String pickedDate);
+}
