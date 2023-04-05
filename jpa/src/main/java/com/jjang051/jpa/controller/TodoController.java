058b12d44cad914c93ae660c020c03d04a7d2387
tisweekendboot <tisweekendboot@gmail.com>
2023-04-05 15:01:47 +0900

    update

---
+++ jpa/src/main/java/com/jjang051/jpa/controller/TodoController.java
@@ -0,0 +1,46 @@
+package com.jjang051.jpa.controller;
+
+import com.jjang051.jpa.entity.TodoJpa;
+import com.jjang051.jpa.service.TodoJpaService;
+import java.util.List;
+import java.util.Optional;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Log4j2
+public class TodoController {
+
+  @Autowired
+  TodoJpaService todoJpaService;
+
+  @GetMapping("/insert")
+  public String insert(TodoJpa todoJpa) {
+    //todoJpaService.insertTodo(todoJpa);
+    return "/todo/insert";
+  }
+
+  @PostMapping("/insert")
+  public String insertProcess(TodoJpa todoJpa) {
+    todoJpaService.insertTodo(todoJpa);
+    return "redirect:/list";
+  }
+
+  @GetMapping("/list")
+  public String list(Model model) {
+    List<TodoJpa> todoList = todoJpaService.getAllTodo();
+    model.addAttribute("todoList", todoList);
+    return "/todo/list";
+  }
+
+  @GetMapping("/view")
+  public String view(int no, Model model) {
+    TodoJpa todoJpa = todoJpaService.getOneTodo(no);
+    model.addAttribute("todoJpa", todoJpa);
+    return "/todo/view";
+  }
+}
--- jpa/src/main/java/com/jjang051/jpa/entity/TodoJpa.java
+++ jpa/src/main/java/com/jjang051/jpa/entity/TodoJpa.java
@@ -1,19 +1,23 @@
 package com.jjang051.jpa.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "TodoTable")
 public class TodoJpa {
 
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private int no;
 
+  @Column(nullable = true, updatable = false, length = 300, unique = true)
   private String todo;
 
   private int done;
